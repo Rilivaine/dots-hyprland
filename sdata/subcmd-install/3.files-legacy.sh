252d935fa9107c004e0f8dd78f5f7164fd4edff8
Rilivaine <Rilivaine@proton.me>
2026-06-30 15:05:45 +0300

    add my vs code and cursor settings

--- sdata/subcmd-install/3.files-legacy.sh
+++ sdata/subcmd-install/3.files-legacy.sh
@@ -8,7 +8,7 @@
 case "${SKIP_MISCCONF}" in
   true) true;;
   *)
-    for i in $(find dots/.config/ -mindepth 1 -maxdepth 1 ! -name 'quickshell' ! -name 'fish' ! -name 'hypr' ! -name 'fontconfig' -exec basename {} \;); do
+    for i in $(find dots/.config/ -mindepth 1 -maxdepth 1 ! -name 'quickshell' ! -name 'fish' ! -name 'hypr' ! -name 'fontconfig' ! -name 'Cursor' ! -name 'Code' -exec basename {} \;); do
 #      i="dots/.config/$i"
       echo "[$0]: Found target: dots/.config/$i"
       if [ -d "dots/.config/$i" ];then install_dir__sync "dots/.config/$i" "$XDG_CONFIG_HOME/$i"
